ac01050cf54fb55d65f9fba36cbd9aaa80b63244
Dennis Kim <dekim2324@gmail.com>
2019-12-31 16:42:51 -0600

    form field on contactform.jsx complete

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactFrom';
 
 const Home = () => {
     return (
         <div className='grid-2'>
             
             <div>
-                {/* Contact Form */}
+                <ContactForm />
             </div>
 
             <div>
