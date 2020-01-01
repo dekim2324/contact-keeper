351c64b5600b5e6f09b15faf271581abcdad835c
Dennis Kim <dekim2324@gmail.com>
2019-12-31 23:02:11 -0600

    working on contactFilter

--- client/src/components/contacts/ContactFrom.jsx
+++ client/src/components/contacts/ContactFrom.jsx
@@ -1,8 +1,22 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactFrom = () => {
-    const contactContext = useContext(ContactContext)
+    const contactContext = useContext(ContactContext);
+
+    const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+    useEffect(() => {
+        if(current !== null) {
+            setContact(current)
+        } else {
+            setContact({       
+            name: '',
+            email: '',
+            phone: '',
+            type: 'personal'
+        })}
+    }, [current])
 
     const [contact, setContact] = useState({
         name: '',
@@ -20,7 +34,11 @@ const ContactFrom = () => {
     const onSubmit = e => {
         e.preventDefault();
         
-        contactContext.addContact(contact);
+        if(current === null) {
+            addContact(contact);
+        } else {
+            updateContact(contact);
+        }
         
         setContact({       
         name: '',
@@ -30,9 +48,14 @@ const ContactFrom = () => {
         })
     }
 
+    const clearAll = () => {
+        clearCurrent();
+
+    }
+
     return (
         <form onSubmit={onSubmit} >
-            <h2 className="text-primary">Add Contact</h2>
+            <h2 className="text-primary">{current ? 'Edit Contact' : 'Add Contact'}</h2>
             <input type="text" placeholder="Name" name="name" value={name} onChange={onChange}/>
             <input type="email" placeholder="Email" name="email" value={email} onChange={onChange}/>
             <input type="text" placeholder="Phone" name="phone" value={phone} onChange={onChange}/>
@@ -40,8 +63,11 @@ const ContactFrom = () => {
             <input type="radio" name="type" value="personal" checked={type === 'personal'} onChange={onChange}/> Personal{' '}
             <input type="radio" name="type" value="professional" checked={type === 'professional'} onChange={onChange}/> Professional{' '}
             <div>
-                <input type="submit" value='Add Contact' className='btn btn-primary btn-block'/>
+                <input type="submit" value={current ? 'Update Contact' : 'Add Contact'} className='btn btn-primary btn-block'/>
             </div>
+            {current && (<div>
+                <button className="btn btn-light btn-block" onClick={clearAll}>Clear</button>
+            </div>)}
         </form>
     )
 }
