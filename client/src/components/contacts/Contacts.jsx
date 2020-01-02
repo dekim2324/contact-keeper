05970549e201fe4edde465fcb2fe56f68518471b
Dennis Kim <dekim2324@gmail.com>
2020-01-01 21:16:36 -0600

    right before integrating the back end

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -13,14 +13,10 @@ const Contacts = () => {
 
     return (
         <Fragment>
-            {filtered !== null ? filtered.map(contact => (<ContactItem key={contact.id}
-             contact={contact} />)) : contacts.map(contact => (
-                <ContactItem key={contact.id} contact={contact} />
-            ))}
-
-            {/* {contacts.map(contact => (
-                <ContactItem key={contact.id} contact={contact} />
-            ))} */}
+                {filtered !== null ? filtered.map(contact =>(<ContactItem key={contact.id} contact={contact} />)) 
+                 : contacts.map(contact => (
+                    <ContactItem key={contact.id} contact={contact} />
+                ))}
         </Fragment>
     )
 }
